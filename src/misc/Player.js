7e755d1d9f5510e78029d6ae4ab62b98be0386c4
Andry <golovchak2000a@gmail.com>
2021-03-15 15:12:29 -0700

    save

--- src/misc/Player.js
+++ src/misc/Player.js
@@ -351,8 +351,7 @@ class Player {
     if (!prevTrack) {
       console.log('Add prev');
       await TrackPlayer.getDuration();
-
-      // await this._init();
+      await this._init();
 
       await TrackPlayer.add({
         id: '-1',
