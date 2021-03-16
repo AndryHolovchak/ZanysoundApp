5fd95318bf9515b053a90dabd4d6febf3e6a1a93
Andry <golovchak2000a@gmail.com>
2021-03-16 17:35:39 +0200

    test

--- src/helpers/WindowHelper.js
+++ src/helpers/WindowHelper.js
@@ -7,6 +7,7 @@ class WindowHelper {
   constructor() {
     this._window = Dimensions.get('window');
     this._screen = Dimensions.get('screen');
+
     this._statusBarHeight = StatusBar.currentHeight;
     this._width = this._window.width;
     this._height = this._window.height;
